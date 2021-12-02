495a5921dbcb98c77e7cf29fe5a46d803ccd2436
gopal_vsspl <gopala.sakshi@vasudhaika.net>
2021-12-02 07:33:37 +0530

    changed path of practice node js files

--- practice22/server1.js
+++ practice22/server1.js
@@ -2,27 +2,27 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {                                      // This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {                  // This responds with "Hello World" on the homepage
    console.log("Got a GET request for the homepage");
    res.send('Hello GET');
 })
 
-app.post('/', function (req, res) {                                     // This responds a POST request for the homepage
+app.post('/', function (req, res) {                  // This responds a POST request for the homepage
    console.log("Got a POST request for the homepage");
    res.send('Hello POST');
 })
 
-app.delete('/del_user', function (req, res) {                              // This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {        // This responds a DELETE request for the /del_user page.
    console.log("Got a DELETE request for /del_user");
    res.send('Hello DELETE');
 })
 
-app.get('/list_user', function (req, res) {                                 // This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {            // This responds a GET request for the /list_user page.
    console.log("Got a GET request for /list_user");
    res.send('Page Listing');
 })
 
-app.get('/ab*cd', function(req, res) {                                  // This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {                  // This responds a GET request for abcd, abxcd, ab123cd, and so on
    console.log("Got a GET request for /ab*cd");
    res.send('Page Pattern Match');
 })
