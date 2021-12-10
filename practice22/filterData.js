58db24a44c7b572221a858583f9f528e4dc84c8d
gopal_vsspl <gopala.sakshi@vasudhaika.net>
2021-12-10 17:32:15 +0530

    exploring mongoose

---
+++ practice22/filterData.js
@@ -0,0 +1,260 @@
+var filter = [
+    {
+        "title": "Date range",
+        "type": "dateRange",
+        "results": [
+            {
+                "title": "Last 30 Days",
+                "key": "Last month",
+                "value": "30_DAYS"
+            },
+            {
+                "title": "Last 7 Days",
+                "key": "Last week",
+                "value": "7_DAYS"
+            },
+            {
+                "title": "Custom Range",
+                "key": "Custom Range",
+                "value": "CUSTOM_RANGE"
+            }
+        ]
+    },
+    {
+        "title": "People",
+        "results": [
+            {
+                "title": "Seller",
+                "type": "sellers",
+                "results": [
+                    {
+                        "key": "Vasudhaika Agritech Private Limited",
+                        "value": "M01na730PRFREG2019103031312545UNH001"
+                    },
+                    {
+                        "key": "Balaji",
+                        "value": "M00kbvr0PRFREG2017121643670611UNH001"
+                    },
+                    {
+                        "key": "Gottuthirupalureddy",
+                        "value": "M00mk6b0PRFREG2018010281471295UNH001"
+                    },
+                    {
+                        "key": "Vinay",
+                        "value": "M01lp7u0PRFREG2019031955330137UNH001"
+                    },
+                    {
+                        "key": "sunnith",
+                        "value": "M01q64v0PRFREG2021040236861959UNH001"
+                    },
+                    {
+                        "key": "Vasudhaika Agritech Private Limited",
+                        "value": "M01q61a0PRFREG2021032620932945UNH001"
+                    },
+                    {
+                        "key": "AG",
+                        "value": "M01oar90PRFREG2020031128666335UNH001"
+                    },
+                    {
+                        "key": "Vinay Rao",
+                        "value": "M00j4b20PRFREG2017020734478472UNH001"
+                    },
+                    {
+                        "key": "Dattaraj Vasudev Kulakarni",
+                        "value": "M01m6sf0PRFREG2019040213979228UNH001"
+                    }
+                ]
+            },
+            {
+                "title": "Owner",
+                "type": "owners",
+                "results": [
+                    {
+                        "key": "Vinay Rao",
+                        "value": "M00j4b20PRFREG2017020734478472UNH001"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "title": "Region",
+        "type": "locations",
+        "results": []
+    },
+    {
+        "title": "Products",
+        "results": [
+            {
+                "title": "Product",
+                "type": "products",
+                "results": [
+                    {
+                        "key": "Tarpaulins",
+                        "value": "O01mfy20CTLGVPRODUCT2019042339335234UNH001"
+                    },
+                    {
+                        "key": "Sprayers",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052949679002UNH001"
+                    },
+                    {
+                        "key": "Farm Machinery",
+                        "value": "O01mkcs0CTLGVPRODUCT2019061020647965UNH001"
+                    },
+                    {
+                        "key": "Veterinary Medicine",
+                        "value": "39hh"
+                    },
+                    {
+                        "key": "Soil Testing",
+                        "value": "O01mker0CTLGVPRODUCT2019061444385456UNH001"
+                    },
+                    {
+                        "key": "Trays",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081253405996UNH001"
+                    },
+                    {
+                        "key": "Hand sprayers",
+                        "value": "O01mkeg0CTLGVPRODUCT2019061243832527UNH001"
+                    },
+                    {
+                        "key": "Neem oil",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081254075390UNH001"
+                    },
+                    {
+                        "key": "DO NOT USE Maize seeds",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081252486393UNH001"
+                    },
+                    {
+                        "key": "Water sprinklers",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052949679842UNH001"
+                    },
+                    {
+                        "key": "Augers",
+                        "value": "O01mkld0CTLGVPRODUCT2019062135434646UNH001"
+                    },
+                    {
+                        "key": "Chainsaw",
+                        "value": "O01mkep0CTLGVPRODUCT2019061340576605UNH001"
+                    },
+                    {
+                        "key": "Milking Machines",
+                        "value": "0oax"
+                    },
+                    {
+                        "key": "DO NOT USE -  Tarpaulins",
+                        "value": "O01mfy20CTLGVPRODUCT2019042339271111UNH001"
+                    },
+                    {
+                        "key": "Sewing Machines",
+                        "value": "3et0"
+                    },
+                    {
+                        "key": "Fruit Picker",
+                        "value": "1lqd"
+                    },
+                    {
+                        "key": "Box",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081253396061UNH001"
+                    },
+                    {
+                        "key": "Fruit Protection Covers",
+                        "value": "0t4q"
+                    },
+                    {
+                        "key": "Mango",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081253953963UNH001"
+                    },
+                    {
+                        "key": "Gromor Spray MKP",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052950152397UNH001"
+                    },
+                    {
+                        "key": "Mango seeds",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081252996414UNH001"
+                    },
+                    {
+                        "key": "Okra seeds or seedlings",
+                        "value": "0lre"
+                    },
+                    {
+                        "key": "Groundnut seeds",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081252934230UNH001"
+                    },
+                    {
+                        "key": "Ethrel®",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052957612456UNH001"
+                    },
+                    {
+                        "key": "Agriculture,Forestry and Garden Hand Tools",
+                        "value": "O01mked0CTLGVPRODUCT2019061226401001UNH001"
+                    },
+                    {
+                        "key": "B/S Paddy Seeds",
+                        "value": "3xrg"
+                    },
+                    {
+                        "key": "Hibiscus seeds",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081253688504UNH001"
+                    },
+                    {
+                        "key": "KK-BBS-318",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052949683381UNH001"
+                    },
+                    {
+                        "key": "Spice seeds",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081251643333UNH001"
+                    },
+                    {
+                        "key": "Raw Mango",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081252083139UNH001"
+                    },
+                    {
+                        "key": "Engines",
+                        "value": "39tw"
+                    },
+                    {
+                        "key": "HTP Sprayers",
+                        "value": "39l8"
+                    },
+                    {
+                        "key": "Battery Sprayer",
+                        "value": "39iu"
+                    },
+                    {
+                        "key": "DO NOT USE Sulphur",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052950172448UNH001"
+                    },
+                    {
+                        "key": "Satsuma mandarin",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081253714425UNH001"
+                    },
+                    {
+                        "key": "Pickles",
+                        "value": "O00k4jn0CTLGVPRODUCT2019081253875583UNH001"
+                    },
+                    {
+                        "key": "Insect Control Traps",
+                        "value": "O00k4jn0CTLGVPRODUCT2019052957449371UNH001"
+                    },
+                    {
+                        "key": "Animal Feed Supplements",
+                        "value": "0pn6"
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+let list = [];
+
+filter.forEach(f => {
+    if (f.results && f.results[0] && f.results[0].value) {
+        list.push(f)
+    }
+});
+
+if(list) {
+    console.log(list[0].results);
+}
--- practice22/hello_node_express.js
+++ practice22/hello_node_express.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   
+   console.log(host);
    console.log("Example app listening at http://%s:%s", host, port)
 })
