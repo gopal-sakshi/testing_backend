383a58c0617aaf16025c2ec8db9a4ee52b06880c
gopal-sakshi <gopal612@gmail.com>
2022-07-17 10:01:16 +0530

    js math object

---
+++ javascript_stuff/Math/2.js
@@ -0,0 +1,33 @@
+/*
+    MATH OBJECT
+    - Unlike other objects, the Math object has no constructor.
+    - The Math object is static.
+    - All methods and properties can be used without creating a Math object first.
+
+    Math Properties 
+    - Math.PI
+    - Math.LN10             // returns the natural logarithm of 10
+
+    Math Methods
+    - Math.round(x)	                    Returns x rounded to its nearest integer
+    - Math.ceil(x)	                    Returns x rounded up to its nearest integer
+    
+    - Math.floor(x)	                    rounds towards closest lower integer number.
+                                        3.5 ---> 3          -3.5 ---> -4
+
+    - Math.trunc(x)	                    Returns the integer part of x (new in ES6)
+                                        just removes the decimals... 
+                                        3.5 ---> 3          -3.5 ---> -3
+
+    - Math.pow(2,5)
+    - Math.sqrt(64)
+    - Math.abs()        ----> returns the absolute positive value               Math.abs(-34.5) 
+    - Math.min(0, 150, 30, 20, -8, -200);
+    - Math.random() returns a random number -------------> between 0, 1
+    
+
+
+
+
+
+*/
---
+++ javascript_stuff/Math/3.js
@@ -0,0 +1,4 @@
+var num21 = -33.4;
+var num22 = 33.65;
+console.log(Math.sign(num21));
+console.log(Math.sign(num22));
