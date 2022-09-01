f6c0b1987fe014b2bfc575f5e88b4e99210e56a7
gopal-sakshi <gopal612@gmail.com>
2022-09-01 13:42:50 +0530

    regex

--- javascript_stuff/regex_highLevel/look_behind_decimal_part.js
+++ javascript_stuff/regex_highLevel/look_behind_decimal_part.js
@@ -6,4 +6,6 @@ var str31 = '31.4231';              // outputs 3, 4231
 var str32 = '3.4231';               // outputs 4231
 var str33 = '3.1.453'               // outputs 453
 var str34 = '3.12.565'              // outputs 1, 565
+var str35 = '3.1.41a443232bcvc34__po{87}';
+var str36 = '3.1.4a1';
 console.log(str34.match(rgx2));
