aa4fe5380c49c91d6f7e59195a2b13b512da2aa5
gopal-sakshi <gopal612@gmail.com>
2022-11-08 13:18:53 +0530

    local time to ISO

---
+++ javascript_stuff/Date_obj/getISO.js
@@ -0,0 +1,7 @@
+var d1 = 'Tue Nov 08 2022 00:00:00 GMT+0530 (India Standard Time)';
+var d22 = new Date('Tue Nov 08 2022 00:00:00 GMT+0530 (India Standard Time)');
+console.log(d22.toISOString());
+
+var t1 = '4:40AM';
+console.log(t1.split(':')[0]);
+console.log(t1.split(':')[1]);
