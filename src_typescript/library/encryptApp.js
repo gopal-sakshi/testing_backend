6537d2ffd709d95b67cc23f8f1b856b071bf0a59
gopal-sakshi <gopal612@gmail.com>
2022-11-15 15:55:54 +0530

    typescript library

---
+++ src_typescript/library/encryptApp.js
@@ -0,0 +1,6 @@
+var encrypt23 = require('gopal612-tslib23');
+var result22 = encrypt23('Hello doctor');
+console.log(result22);
+
+
+
---
+++ src_typescript/library/encryptApp.ts
@@ -0,0 +1,4 @@
+var encrypt23 = require('gopal612-tslib23');
+
+var result22 = encrypt23('Hello doctor');
+console.log(result22);
