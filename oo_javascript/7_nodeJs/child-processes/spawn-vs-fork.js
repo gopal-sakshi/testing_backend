59863ae0efba0981831a9604bd3b120414864aae
gopal-sakshi <gopal612@gmail.com>
2022-12-01 21:48:49 +0530

    ss

--- oo_javascript/7_nodeJs/child-processes/spawn-vs-fork.js
+++ oo_javascript/7_nodeJs/child-processes/spawn-vs-fork.js
@@ -4,7 +4,8 @@ SPAWN
     (A) the child process will be run as its own process.
     doesnt execute any further code within your node process
     You can add listeners for the child_process you have spawned, to allow your parent_process code interact with the spawned child_pprocess
-    no new V8 instance is created
+    no new V8 instance is created... unless of course your command is another Node command
+        but if your command is another node command ---> you should probably use fork...
     only one copy of your node module is active on the processor.
 
     (B) when you want to make a continuous data transfer (1Gb video file)
@@ -16,6 +17,16 @@ FORK
     meaning, we can create multiple workers (or) worker pool 
 
     (B) send individual messages — JSON
+
+    fork = special instance of spawn, 
+    that runs a fresh instance of the V8 engine. 
+    Meaning, you can essentially create multiple workers, running on the exact same Node code base, or perhaps a different module for a specific task. 
+    This is most useful for creating a worker pool. 
+    While node's async event model allows a single core of a machine to be used fairly efficiently, 
+        it doesn't allow a node process to make use of multi core machines. 
+    Easiest way to accomplish this is to run multiple copies of the same program, on a single processor.
+
+
 
 
 
