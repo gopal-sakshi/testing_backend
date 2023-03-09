edfb8f09a847285921e1c3445eae02cf3614d3d1
gopal-sakshi <gopal612@gmail.com>
2023-03-09 07:51:40 +0530

    rxjs in node, not angular

--- oo_javascript/2_nextLevel/rxjs2/1.js
+++ oo_javascript/2_nextLevel/rxjs2/1.js
@@ -6,7 +6,7 @@ const clock$ = Observable.create((subject) => {
         subject.next('tick');
     }, 1000);
     setTimeout(() => clearInterval(interval), 7 * 1000);
- });
- const subscription = clock$.subscribe(console.log);
-  
- setTimeout(() => subscription.unsubscribe(), 10 * 1000);
+});
+const subscription = clock$.subscribe(console.log);
+
+setTimeout(() => subscription.unsubscribe(), 10 * 1000);
