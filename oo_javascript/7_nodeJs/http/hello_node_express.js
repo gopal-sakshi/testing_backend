5604c997f0f4a5b98db072ed22b681fcf01535cf
gsakshi <gsakshi@kpt.com>
2024-08-22 09:07:05 +0530

    how come 2 lodash present in package.json

--- oo_javascript/7_nodeJs/http/hello_node_express.js
+++ oo_javascript/7_nodeJs/http/hello_node_express.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+app.use(cors())
 
 app.get('/', function (req, res) {
    res.send('Hello World --> served using express APP');
-})
+});
+
+app.get('/json23', function (req, res) {
+    res.send({
+        time23: new Date().toISOString(),
+        info23: 'sending json resp back'
+    });
+ });
+
+app.post('/react45', async (req, res) => {
+    res.send({
+        time23: new Date().toISOString(),
+        ...req.body,
+        info23: 'sending back req body'
+    })
+});
 
 var server = app.listen(8081, function () {
    var host = server.address().address
